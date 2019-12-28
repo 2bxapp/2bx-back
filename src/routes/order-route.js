40d0e20f2b1ad58ccdc286ed97c356cdd6d101c7
Emanuel Vieira <emanuel_vieira@outlook.com>
2019-12-27 21:12:08 -0300

    get order by status

--- src/routes/order-route.js
+++ src/routes/order-route.js
@@ -7,6 +7,7 @@ const authService = require('../services/auth-service');
 
 router.get('/', controller.get);
 router.get('/:id', controller.getById);
+router.get('/status/:status', controller.getByStatus);
 router.post('/', controller.post);
 router.patch('/', controller.patch);
 
